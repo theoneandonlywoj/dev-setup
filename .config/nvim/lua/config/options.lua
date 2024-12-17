1678d1c2e47537e1b374fa0e6316b7b3e9ec759e
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-12-17 23:22:14 +0100

    refactor(Neovim Options).

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -19,39 +19,58 @@ vim.g.root_spec = { "lsp", { ".git", "lua", ".mix" }, "cwd" }
 -- Do NOT hide deprecation warnings
 vim.g.deprecation_warnings = false
 
+-- Limiting the waiting time to 500ms
+vim.opt.timeoutlen = 500
+
+-- Number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 8
+
+-- Better editor UI
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes:1'
+vim.opt.cursorline = true
+
+-- More clear text formatting
+vim.opt.list = true
+vim.opt.listchars = 'space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,leadmultispace:│   ,'
+
+-- Remember 50 items in commandline history
+vim.opt.history = 50
+
+-- Preserve view while jumping
+vim.opt.jumpoptions = 'view'
+
 -- 
 vim.opt.complete = ""
--- opt = vim.o
--- vim.api.nvim_set_option("clipboard", "unnamedplus")
--- Clipboard
--- opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+
+-- Confirm to save changes before exiting modified buffer
+vim.opt.confirm = true
 --
--- -- Confirm to save changes before exiting modified buffer
--- opt.confirm = true
---
--- -- Use spaces instead of tabs
--- opt.expandtab = true
---
--- -- Enable mouse mode
--- opt.mouse = "a"
---
--- -- Print line number
--- opt.number = true
---
--- -- Round indent
--- opt.shiftround = true
---
--- -- Size of an indent
--- opt.shiftwidth = 2
---
--- -- Dont show mode since we have a statusline
--- opt.showmode = false
---
--- -- Spelling
--- opt.spelllang = { "en" }
---
--- -- Number of spaces tabs count for
--- opt.tabstop = 2
---
--- -- True color support
--- opt.termguicolors = true
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Print line number
+vim.opt.number = true
+
+-- Round indent
+vim.opt.shiftround = true
+
+-- Size of an indent
+vim.opt.shiftwidth = 2
+
+-- Dont show mode since we have a statusline
+vim.opt.showmode = false
+
+-- Spelling
+vim.opt.spelllang = { "en" }
+
+-- Number of spaces tabs count for
+vim.opt.tabstop = 2
+
+-- True color support
+vim.opt.termguicolors = true

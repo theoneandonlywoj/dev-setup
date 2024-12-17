5fb8c19466428baaf61978778923f8a0569e8a21
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-12-17 23:21:36 +0100

    feat(Treesitter Parsers).

--- .config/nvim/lua/plugins/treesitter_parsers.lua
+++ .config/nvim/lua/plugins/treesitter_parsers.lua
@@ -1,21 +1,20 @@
-return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-          ensure_installed = {
-            "bash",
-            "html",
-            "javascript",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "elixirls",
-            "query",
-            "regex",
-            "vim",
-            "yaml",
-          },
-        },
-      },
-}
+-- return {
+--     {
+--         "nvim-treesitter/nvim-treesitter",
+--         opts = {
+--           ensure_installed = {
+--             "bash",
+--             "html",
+--             "javascript",
+--             "json",
+--             "lua",
+--             "markdown",
+--             "markdown_inline",
+--             "query",
+--             "regex",
+--             "vim",
+--             "yaml",
+--           },
+--         },
+--       },
+-- }

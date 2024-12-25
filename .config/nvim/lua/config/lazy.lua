5ec466d30f694bececc689a538f8f2651a9f8039
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-12-25 13:36:09 +0100

    chore(deleting obsolete configs)

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -41,9 +41,6 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",

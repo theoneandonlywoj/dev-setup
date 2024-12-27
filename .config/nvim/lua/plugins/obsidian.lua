d170331c926864ab0b0e68571c5a25da5765f721
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-12-27 17:39:20 +0100

    bug(New Obsidian Note error): fix based on Github comment.

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -23,7 +23,7 @@ return {
             }
         },
         -- Optional, if you keep notes in a specific subdirectory of your vault.
-        notes_subdir = "notes",
+        notes_subdir = "1.Notes",
         -- Optional, customize how note file names are generated given the ID, target directory, and title.
         ---@param spec { id: string, dir: obsidian.Path, title: string|? }
         ---@return string|obsidian.Path The full path to the new note.
@@ -37,7 +37,7 @@ return {
         log_level = vim.log.levels.INFO,
         daily_notes = {
             -- Optional, if you keep daily notes in a separate directory.
-            folder = "notes/daily",
+            folder = "3.DailyNotes",
             -- Optional, if you want to change the date format for the ID of daily notes.
             date_format = "%Y-%m-%d",
             -- Optional, if you want to change the date format of the default alias of daily notes.
@@ -49,11 +49,13 @@ return {
         },
         -- Optional, for templates (see below).
         templates = {
-            folder = "templates",
+            folder = "2.Templates",
             date_format = "%Y-%m-%d",
             time_format = "%H:%M",
             -- A map for custom variables, the key should be the variable and the value a function
             substitutions = {}
-        }
+        },
+        -- https://github.com/nvim-tree/nvim-tree.lua/issues/925#issuecomment-1475085321
+        update_focused_file = { enable = true, update_cwd = true, --[[ ignore_list = { "/node_modules" }, ]] },
     }
 }

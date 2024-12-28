37cd0bd62fc14a43654e3d3f6965eafac0f467bc
theoneandonlywoj <theoneandonlywoj@gmail.com>
2024-12-28 09:52:30 +0100

    bug(Obsidian): loading obsidian.nvim on startup

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
+    lazy = false,
     ft = "markdown",
     dependencies = {
         -- Required.

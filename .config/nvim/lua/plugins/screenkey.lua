69ca26c1b576e10b75d934633ed242fe47f563d4
theoneandonlywoj <theoneandonlywoj@gmail.com>
2025-01-12 01:17:33 +0100

    feat(config): Screenkey config.

--- .config/nvim/lua/plugins/screenkey.lua
+++ .config/nvim/lua/plugins/screenkey.lua
@@ -2,4 +2,12 @@ return {
   "NStefan002/screenkey.nvim",
   version = "*",
   lazy = false,
+  config = function()
+    require("screenkey").setup({
+      compress_after = 5,
+      clear_after = 5,
+      show_leader = true,
+      group_mappings = true,
+    })
+  end,
 }
